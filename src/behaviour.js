587f2bfd09a80211c10926ffd79725013794dfee
dudematthew <mmoczydlowski4@gmail.com>
2021-02-02 11:26:55 +0100

    Change welcome image font; Adjust welcome image

--- src/behaviour.js
+++ src/behaviour.js
@@ -87,6 +87,9 @@ export default class Behaviour {
           Jsonfile.readFile('./private.json', function (err, obj) {
             let API_KEY, API_ID;
 
+            // Retrieve your api_id and api_key
+            // from the Dashboard. 
+            // https://htmlcsstoimage.com/dashboard
             if (err) {
               API_KEY = process.env.HTCI_API_KEY;
               API_ID = process.env.HTCI_API_ID;
@@ -95,13 +98,15 @@ export default class Behaviour {
               API_ID = obj.HTCI_API_ID ?? process.env.HTCI_API_ID;
             }
 
-            // Create an image by sending a POST to the API.
-            // Retrieve your api_id and api_key from the Dashboard. https://htmlcsstoimage.com/dashboard
+            // Create an image by sending 
+            // a POST to the API.
             request
               .post({ url: 'https://hcti.io/v1/image', form: data })
               .auth(API_ID, API_KEY)
               .on('data', function (data) {
                 let imgLink = JSON.parse(data).url;
+                // Send welcome image to welcome
+                // channel
                 welcomeChannel.send({
                   files: [`${imgLink}.png`],
                 });
